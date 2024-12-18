bff3f6bf06549ba19a4ed6a2d3a8b873e1b931d1
KoMartinkk <kokinkwanmartin@gmail.com>
2024-12-18 23:28:13 +0800

    18/12

---
+++ fundingrateBT.py
@@ -0,0 +1,38 @@
+import ccxt
+import time
+import pandas as pd
+import numpy as np
+import datetime
+#from tools import 
+import os
+from pprint import pprint
+import requests
+
+exchange = ccxt.bybit({
+    'apiKey': 'tGnj6inSl01LwTWnZ8',  
+    'secret': 'qgQdKexzvAkzYxlastwA0FAQP3Rqi4aYL16X',
+    'options': {
+        'adjustForTimeDifference': True,
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
